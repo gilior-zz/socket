46f9f2863615f88be388667a8aebaf68722de38c
LiorG <liorgish@gmail.com>
2017-11-22 15:25:16 +0200

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isActive: boolean = true;
+  public msgs: Array<string>
+  public dummy: Array<string>
+  constructor(private ref: ChangeDetectorRef) {
+    // this.dummy = new Array<string>();
+    // this.dummy.push('dfdf', 'dfdfdf');
+    var connection = $.hubConnection('http://localhost:58949');
+    var proxy = connection.createHubProxy('MyHub1');
+    this.msgs = new Array<string>();    
+    // receives broadcast messages from a hub function, called "broadcastMessage"
+    proxy.on('Hello', (i) => {
+      console.log(i);      
+      this.msgs.push(i);     
+      console.log(this.msgs);
+      ref.detectChanges();
+    });
+    // atempt connection, and handle errors
+    connection.start({ jsonp: true })
+      .done(function () { console.log('Now connected, connection ID=' + connection.id); proxy.invoke('Hello') })
+      .fail(function () { console.log('Could not connect'); });
+  }
+
+ 
+  
 }
